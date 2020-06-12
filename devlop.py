fcd64d138c33f89b1a19d6495808c7e5f1ad7754
Hibiki Okada <4617016@ed.tus.ac.jp>
2020-06-12 19:16:33 +0900

    confirm to PEP8 coding standard

--- devlop.py
+++ devlop.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -10,12 +11,14 @@ class Application(tk.Frame):
     def create_widgets(self):
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
- #       self.hi_there["command"] = self.say_hi
+        # self.hi_there["command"] = self.say_hi
         self.hi_there.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         self.quit.pack(side="bottom")
+
+
 '''
     def say_hi(self):
         print("hello everyone!")
